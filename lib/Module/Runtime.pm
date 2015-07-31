bc1847164021b7c88a52b0d4077d7c6276851b3e
Tim Orling <TicoTimo@gmail.com>
2015-07-30 21:31:11 -0700

    Convert to EUMM

--- lib/Module/Runtime.pm
+++ lib/Module/Runtime.pm
@@ -120,7 +120,7 @@ BEGIN { require 5.006; }
 BEGIN { ${^WARNING_BITS} = ""; }
 # Don't "use strict" here, to avoid dependencies.
 
-our $VERSION = "0.014";
+our $VERSION = "0.015";
 
 # Don't use Exporter here, to avoid dependencies.
 our @EXPORT_OK = qw(
@@ -489,6 +489,8 @@ L<perlfunc/use>
 =head1 AUTHOR
 
 Andrew Main (Zefram) <zefram@fysh.org>
+
+Tim Orling (Timo) <ticotimo@gmail.com>
 
 =head1 COPYRIGHT
 
